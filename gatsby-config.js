8c0b9d3e3211e48892e6020f7adb9be8d606b438
Warren Partridge <maps@perdix.org>
2022-06-22 20:17:34 -0700

    Add Gatsby template

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Perdicis`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Hello</h1>
+      <p>Spaghetti text</p>
+    </main>
+  )
+}
+
+export default IndexPage
